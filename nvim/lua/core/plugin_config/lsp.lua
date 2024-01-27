9f1ac15cbb1229176fa1d064ba87fd1e08d0fbe6
Amr Moussa <amrmoussa100@gmail.com>
2024-01-27 13:16:52 -0600

    add rust support to my nvim lsp

--- nvim/lua/core/plugin_config/lsp.lua
+++ nvim/lua/core/plugin_config/lsp.lua
@@ -73,4 +73,9 @@ require("lspconfig").gopls.setup {
   capabilities = capabilities,
 }
 
+require("lspconfig").rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 
--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"c", "cpp", "lua", "rust", "vim", "python", "erlang"},
-  
+  ensure_installed = {"c", "cpp", "lua", "rust", "vim", "python", "erlang", "javascript"},
   sync_install = false,
   auto_install = false,
   highlight = {
