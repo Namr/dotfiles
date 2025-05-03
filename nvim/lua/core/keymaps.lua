2c4c4cee9c3360ffe144d68661c6f9c41b8b3ea3
Amr Moussa <amoussa@relaypro.com>
2025-05-03 11:00:27 -0400

    neovim config changes

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,7 +14,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.cindent = true
 vim.opt.scrolloff = 8
 
 -- remappings
